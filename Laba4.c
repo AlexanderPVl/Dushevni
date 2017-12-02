aef73ffb121c468a8592cdce18c603d12f793b72
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2017-12-02 20:51:20 +0400

    Create Laba4.c

---
+++ Laba4.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void scan(int *k){
+	signed int sign = 1;
+	int r = 0;
+	char c;
+	while ((((c = getchar()) == ' ') || (c - '0' < 0) || (c - '0' > 9)) && (c != '-'));
+	if (c == '-')
+		sign = -1;
+	if ((c - '0' >= 0) && (c - '0' <= 9))
+		r += c - '0';
+	while ((c = getchar()) != '\n' && (c != ' ')){
+		if ((c - '0' >= 0) && (c - '0' <= 9)){
+			r = r * 10 + sign*(c - '0');
+		}
+	}
+	*k = r;
+}
+
+int InputArray(int *adres, int count){
+	if (adres == NULL){
+		printf("\nADRES = NULL\n");
+		return (-1);
+	}
+	printf("\nEnter Array: ");
+	int k, i = 0;
+	for (i = 0; i < count; i++){
+		scan(&k);
+		*(adres + i) = k;
+	}
+	return (0);
+}
+
+int PrintArray(int *adres, int count){
+	if (adres == NULL){
+		printf("\nADRES = NULL\n");
+		return (-1);
+	}
+	int i;
+	for (i = 0; i < count; i++)
+		printf("%d ", *(adres + i));
+	printf("\n");
+	return (0);
+}
+
+int Solution(int *adres, int *count){
+	if (adres == NULL){
+		printf("\nADRES = NULL\n");
+		return (-1);
+	}
+	int max, i = 0, ind = 0, c, j, excess = 0;
+	for (max = *adres; i < *count; i++){
+		if ((c = *(adres + i)) >= max){
+			ind = i;
+			max = c;
+		}
+	}
+	for (i = ind - 1; i >= 0; i--)
+		if (*(adres + i) % 2 == 1){
+		for (j = i; j < *count; j++)
+			*(adres + j) = *(adres + j + 1);
+		*count -= 1;
+		}
+	return (0);
+}
+
+int main(){
+	int *adres;
+	int k = 0;
+	printf("Enter number of elements: ");
+	do{
+		scan(&k);
+		if (k > 0 && k <= 20)
+			break;
+	} while (k <= 0, printf("Enter positive number under 20 (you entered %d): ", k));
+	printf("k = %d\n", k);
+	adres = (int*)malloc(k*sizeof(int));
+
+	InputArray(adres, k);
+	printf("\nYour array:\n");
+	PrintArray(adres, k);
+
+	Solution(adres, &k);
+	printf("\nResult array:\n");
+	PrintArray(adres, k);
+
+	free(adres);
+	return 0;
+}

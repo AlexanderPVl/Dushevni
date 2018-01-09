c8d249c9841288baa0a6fcabe4ab23ad58b5348b
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-01-09 07:51:57 +0400

    Update AssemblerProj1.c

--- AssemblerProj1.c
+++ AssemblerProj1.c
@@ -1,25 +1,60 @@
 #include <stdio.h>
 
-void main()
+void EnterNums(int* x, int* y, int* z)
 {
-	char y, z;
-	short int x, v;
-	scanf_s("%d%d%d", &x, &y, &z);
+	do{
+		printf("Enter x:\n");
+		scanf_s("%d", x);
+	} while (*x < -32767 || *x > 32767);
+	do{
+		printf("Enter y:\n");
+		scanf_s("%d", y);
+	} while (*y < -127 || *y > 127 || *y == 1);
+	do{
+		printf("Enter z:\n");
+		scanf_s("%d", z);
+	} while (*z < -127 || *z > 127);
+}
+
+int Calc(short int x, char y, char z)
+{
+	printf("x = %d\n", x);
+	printf("y = %d\n", y);
+	printf("z = %d\n", z);
+	short int v = 0;
 
 	__asm
 	{
+		mov eax, 0
+		add z, -3
+		add y, -1
 		mov al, z
-		add al, -3
-		mov ah, 0
-		mov bx, x
-		mul bx
-		add ax, 1
-		mov bl, y
-		mov bh, 0
-		add bx, -1
-		div bx
-		add ax, 1
+		cbw
+		mul x // результат умножения запишется в eax
+		add eax, 1
+		mov edx, 0
+		mov ebx, eax
+		mov eax, 0
+		mov al, y
+		cbw
+		cwde
+		xchg eax, ebx
+		div ebx
+		add eax, 1
+
 		mov v, ax
 	}
-	printf("%d", v);
+	printf("v = %d\n", v);
+
+	return v;
+}
+
+void main()
+{
+	int y, z; // byte
+	int x, v; // word
+
+	//EnterNums(&x, &y, &z);
+	v = Calc(-3, 3, 6);
+	//v = Calc(2835, 37, -126);
 }

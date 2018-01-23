bad0fadab0eb4007b772121fc7ab829a46960802
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-01-23 10:44:00 +0300

    Create Labora5.c

---
+++ Labora5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef char* string;
+
+typedef struct
+{
+	int Ind;
+	int Leng;
+} Word;
+
+typedef struct
+{
+	Word* WordArr;
+	int CouWord;
+	string Str;
+} String;
+
+
+
+int main()
+{
+	String Str;
+	return 0;
+
+
+}

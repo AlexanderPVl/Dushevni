14a1a748e25ec871cd2e5b74ef8f8f76be88f15a
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-01-31 13:46:32 +0300

    Update AssemblerProj1.c

--- AssemblerProj1.c
+++ AssemblerProj1.c
@@ -1,60 +1,42 @@
 #include <stdio.h>
 
-void EnterNums(int* x, int* y, int* z)
-{
-	do{
-		printf("Enter x:\n");
-		scanf_s("%d", x);
-	} while (*x < -32767 || *x > 32767);
-	do{
-		printf("Enter y:\n");
-		scanf_s("%d", y);
-	} while (*y < -127 || *y > 127 || *y == 1);
-	do{
-		printf("Enter z:\n");
-		scanf_s("%d", z);
-	} while (*z < -127 || *z > 127);
-}
-
 int Calc(short int x, char y, char z)
 {
+	printf("\nRESULTS:\n");
 	printf("x = %d\n", x);
 	printf("y = %d\n", y);
 	printf("z = %d\n", z);
-	short int v = 0;
-
+	short int v_asm = 0;
+	short int v_c = 0;
+	v_c = ((z - 3)*x + 1) / (y - 1) + 1;
 	__asm
 	{
-		mov eax, 0
-		add z, -3
-		add y, -1
+		mov eax, 0 // clean register
+		sub z, 3
 		mov al, z
 		cbw
-		mul x // результат умножения запишется в eax
-		add eax, 1
-		mov edx, 0
-		mov ebx, eax
-		mov eax, 0
+		imul x // результат умножения запишется в eax
+		add ax, 1
+		mov bx, ax
+		sub y, 1
 		mov al, y
 		cbw
-		cwde
-		xchg eax, ebx
-		div ebx
-		add eax, 1
-
-		mov v, ax
+		xchg ax, bx
+		idiv bx
+		add ax, 1
+		mov v_asm, ax
 	}
-	printf("v = %d\n", v);
-
-	return v;
+	printf("Assembler calculation: v_asm = %d\n", v_asm);
+	printf("C calculation: v_c = %d\n", v_c);
+	return v_asm;
 }
 
 void main()
 {
-	int y, z; // byte
-	int x, v; // word
+	int v; // word
 
-	//EnterNums(&x, &y, &z);
+
+
 	v = Calc(-3, 3, 6);
-	//v = Calc(2835, 37, -126);
+	v = Calc(2835, 37, -126);
 }

a5818046fbc58bd1e50e52f90ca15ad2f27e988a
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-01 01:19:48 +0400

    Update AssemblerProj1.c

--- AssemblerProj1.c
+++ AssemblerProj1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int Calc(short int x, char y, char z)
+int Calc(signed short int x, signed char y, signed char z)
 {
 	printf("\nRESULTS:\n");
 	printf("x = %d\n", x);
@@ -11,23 +11,29 @@ int Calc(short int x, char y, char z)
 	v_c = ((z - 3)*x + 1) / (y - 1) + 1;
 	__asm
 	{
-		mov eax, 0 // clean register
-		sub z, 3
-		mov al, z
-		cbw
-		imul x // результат умножения запишется в eax
-		add ax, 1
-		mov bx, ax
-		sub y, 1
-		mov al, y
-		cbw
-		xchg ax, bx
-		idiv bx
-		add ax, 1
-		mov v_asm, ax
+		mov eax, 0;
+		mov ebx, 0;
+		mov ecx, 0;
+		mov edx, 0;
+		mov al, z;
+		cbw;
+		sub al, 3;
+		//cbw;
+		mov bx, x
+		imul bx; // результат умножения запишется в eax
+		add ax, 1;
+		adc dx, 0;
+		mov bx, ax;
+		mov al, y;
+		cbw;
+		sub ax, 1
+		xchg ax, bx;
+		idiv bx;
+		add ax, 1;
+		mov v_asm, ax;
 	}
-	printf("Assembler calculation: v_asm = %d\n", v_asm);
-	printf("C calculation: v_c = %d\n", v_c);
+	printf("Assembler calculation: v_asm = %x\n", v_asm);
+	printf("C calculation: v_c = %x\n", v_c);
 	return v_asm;
 }
 
@@ -37,6 +43,6 @@ void main()
 
 
 
-	v = Calc(-3, 3, 6);
-	v = Calc(2835, 37, -126);
+	v = Calc(-0x3, 0x3, 0x6);
+	v = Calc(0xB13, 0x25, -0x7E);
 }
